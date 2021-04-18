f8821230194dfbee95f5cae7a6c5c9e412149026
Ferney Mejia <ferneymejiap@gmail.com>
2021-04-17 23:13:42 -0500

    Primer avance en el código e informe

--- Codigo/Parcial1/main.cpp
+++ Codigo/Parcial1/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
 using namespace std;
+const int SER = 13;
+const int RCLK = 12;
+const int SRCLK = 11;
 
 int main()
-{
-    cout << "Hello World!" << endl;
+{     
     return 0;
+}
+
+void loop()
+{
+  digitalWrite(SER, LOW);
+  digitalWrite(RCLK, LOW);
+  digitalWrite(SRCLK, LOW);
+
+  digitalWrite(SER, HIGH);
+
+  digitalWrite(SRCLK, LOW);
+  digitalWrite(SRCLK, HIGH);
+  digitalWrite(SRCLK, LOW);
+
+  digitalWrite(RCLK, LOW);
+  digitalWrite(RCLK, HIGH);
+  digitalWrite(RCLK, LOW);
 }
